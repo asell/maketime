f45de0de4db50cfe68bc500e0c0018bd2fc9ef0a
holtc <holt.chris19@gmail.com>
2014-09-14 08:51:54 -0400

    Update autoschedule.js
    newest code after refactor

--- autoschedule.js
+++ autoschedule.js
@@ -3,7 +3,7 @@
  */
 //creates a moment from a date string
 var createMoment = function(dateString){
-	return moment(dateString, 'YYYY-MM-DD HH:mm');
+    return moment(dateString);
 }
 
 //returns -1 if x is before y in minutes, 0 if same, 1 if after
@@ -13,10 +13,10 @@ var sortOnStartDate = function(firstEvent, secondEvent){
 	var secondDateTime = secondEvent.start.dateTime;
 	var secondDateEndTime = secondEvent.end.dateTime;
 	if(!firstDateTime){
-		firstDateTime = String(firstEvent.start.date);
+		firstDateTime = firstEvent.start.date;
 	}
 	if(!firstDateEndTime){
-		firstDateEndTime = String(firstEvent.end.date);
+		firstDateEndTime = firstEvent.end.date;
 	}
 	if(!secondDateTime){
 		secondDateTime = secondEvent.start.date;
@@ -30,20 +30,29 @@ var sortOnStartDate = function(firstEvent, secondEvent){
 }
 
 //returns a list of all calendar events from google overlapping at all with startBy-endBy range
+//does not need to be sorted
 var filterGoogleEvents = function(startByDateTime, endByDateTime, eList){
 	var filteredEventList = [];
 	var startBy = createMoment(startByDateTime);
 	var endBy = createMoment(endByDateTime)
-	for (event in eList){
-		var curEventStart = createMoment(eList[event].start.dateTime);
-		var curEventEnd = createMoment(eList[event].end.dateTime);
+	for (index in eList){
+		var eventStartDateTime = eList[index].start.dateTime;
+		var eventEndDateTime = eList[index].end.dateTime;
+		if (!eventStartDateTime){
+			eventStartDateTime = eList[index].start.date;
+		}
+		if (!eventEndDateTime){
+			eventEndDateTime = eList[index].end.date;
+		}
+		var eventStart = createMoment(eventStartDateTime);
+		var eventEnd = createMoment(eventEndDateTime);
 		
 		//does currentEvent overlap with the range at all
 		//if so, add it to the list of events in the range
-		if (((curEventStart.isAfter(startBy, 'minute') || curEventStart.isSame(startBy, 'minute')) && (curEventStart.isBefore(endBy, 'minute'))) 
-			  || ((curEventEnd.isAfter(startBy, 'minute') || curEventEnd.isSame(startBy, 'minute')) && (curEventEnd.isBefore(endBy, 'minute'))))
+		if (((eventStart.isAfter(startBy, 'minute') || eventStart.isSame(startBy, 'minute')) && (eventStart.isBefore(endBy, 'minute'))) 
+			  || ((eventEnd.isAfter(startBy, 'minute') || eventEnd.isSame(startBy, 'minute')) && (eventEnd.isBefore(endBy, 'minute'))))
 		{
-			filteredEventList.push(eList[event]);
+			filteredEventList.push(eList[index]);
 		}
 	}
 	return filteredEventList;
@@ -135,8 +144,16 @@ var calculateAvailableSlots = function(ourTask, filteredGoogleCalEventList){
 	//see if any slots before each event and the prior event, if so add to slot list
 	for (var i = 0; i < filteredGoogleCalEventList.length; i++){
 		var event = filteredGoogleCalEventList[i];
-		var eventStart = createMoment(event.start.dateTime);
-		var eventEnd = createMoment(event.end.dateTime);
+		var eventStartDateTime = event.start.dateTime;
+		var eventEndDateTime = event.end.dateTime;
+		if (!eventStartDateTime){
+			eventStartDateTime = event.start.date;
+		}
+		if (!eventEndDateTime){
+			eventEndDateTime = event.end.date;
+		}
+		var eventStart = createMoment(eventStartDateTime);
+		var eventEnd = createMoment(eventEndDateTime);
 		//if we can fit our task between events, add the slot
 		if ((eventStart.diff(pastEventEnd, 'minutes') >= ourTask.length)){
 			slot = createSlot(pastEventEnd.format(), eventStart.format());
@@ -153,62 +170,24 @@ var calculateAvailableSlots = function(ourTask, filteredGoogleCalEventList){
 	return availableSlots;
 };
 
-//uses new logic, testing currently, sets task actual and returns an event in google cal form to send
-var scheduleNewAlgorithm = function(ourTask, availableSlots){
-	var slot = availableSlots[0];
-	ourTask.actual = slot.start;
-	return taskToEvent(ourTask);
+//uses new logic, testing currently, sets task actual and returns a json object with task to store in db and event to push to google cal
+//slotPicker is a function to pass in that takes in a list of slots and returns which to use
+var scheduleNewTask = function(ourTask, availableSlots, slotPicker){
+	var slot = slotPicker(availableSlots);
+    ourTask.actual = slot.start;
+    return {"task": ourTask, "event": taskToEvent(ourTask)};
 }
 
-//returns the event with all fields properly filled out, throws error if can't create
-//schedules in earliest available slot after the startBy dateTime
-//filtered and sorted list
-var scheduleEventFirstAvailable = function(ourEvent, filteredSortedGoogleCalEventList){
-	var prevEvent = filteredGoogleCalEventList[0];
-	var prevEventStart = createMoment(prevEvent.start.dateTime);
-	var prevEventEnd = createMoment(prevEvent.end.dateTime);
-	var event;
-	//if we can fit before first item in range, or if the list is empty
-	//then set the time fields and return the event
-	//TODO: add check to take max of (ourEvent.startBy, actual time.now (moment.js now() maybe?))
-	if (((prevEvent.start.dateTime >= ourEvent.startBy) && ((prevEvent.start.dateTime - ourEvent.startBy) >= ourEvent.duration)) || filteredGoogleCalEventList.length <= 0){
-		event = {"kind": "calendar#event", "id": ourEvent.id, "start": ourEvent.startBy, "end": ourEvent.startBy + ourEvent.duration};
-		return event;
-	}
-	//if length 1
-	if (filteredGoogleCalEventList.length === 1){
-		//if we can fit after first item
-		if ((ourEvent.endBy.dateTime - prevEvent.end.dateTime) >= ourEvent.duration){
-			event = {"kind": "calendar#event", "id": ourEvent.id, "start": prevEvent.end, "end": prevEvent.end + ourEvent.duration};
-			return event;
-		}//can't fit, so return null
-		else return null;
-	}
-	for (var i = 1; i < googleCalEventList.length; i++){
-		var currEvent = googleCalEventList[i];
-		if (ourEvent.duration <= (currEvent.start.dateTime - prevEvent.end.dateTime)){
-			var start;
-			var end;
-			//TODO: add check to take max of (ourEvent.startBy, actual time.now (moment.js now() maybe?)) needs work for re-schedule
-			if (ourEvent.startBy < prevEvent.end.dateTime){
-				start = prevEvent.end;
-				end = start + duration;
-			}
-			else {
-				start = ourEvent.startBy;
-				end = start;
-			}
-			event = {"kind": "calendar#event", "id": ourEvent.id, "start": start, "end": end};
-			return event;
-		}
-		prevEvent = currEvent;
-	}
-	//check to see if can add after the last event
-	//TODO: add check to take max of (ourEvent.startBy, actual time.now (moment.js now() maybe?))
-	if ((ourEvent.endBy.dateTime - lastEndTime) >= ourEvent.duration){
-		event = {"kind": "calendar#event", "id": ourEvent.id, "start": prevEvent.end, "end": prevEvent.end + ourEvent.duration};
-		return event;
-	}
-	//can't add it
-	return null;
-};
+var autoSchedule = function(ourTask, googleCalendarEventList, schedulingAlgorithm){
+	googleCalendarEventList.sort(sortOnStartDate);
+	filtered = filterGoogleEvents(ourTask.date.set.start, ourTask.date.set.end, googleCalendarEventList);
+	availableSlots = calculateAvailableSlots(ourTask, filtered);
+	map = scheduleNewTask()
+}
+
+//chooses first available slot to schedule event
+var pickFirstAvailable = function(availableSlots){
+	return availableSlots[0];
+}
+
+var pickLastAvailable
